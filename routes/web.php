bcbd571145c8e87fe221950dbdc256b5c72fb201
Maréme Thiaw <mariamathiaw92@gmail.com>
2025-06-02 17:22:51 +0100

    Création de l'admin par seeder et test de la connexion de l'admin

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,14 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', AdminMiddleware::class])->group(function () {
+    Route::get('/admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
+    Route::resource('/admin/agences', AgenceController::class);
+});
+/*Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+});*/
 
 require __DIR__.'/auth.php';
